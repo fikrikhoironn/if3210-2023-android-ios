ca0ed857d8ee659828916cb5d82cbd6d17b8e475
jasonk19 <13520080@std.stei.itb.ac.id>
2023-02-14 16:20:32 +0700

    feat: cabang restoran layout

--- app/src/main/java/com/example/majika/MainActivity.kt
+++ app/src/main/java/com/example/majika/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.majika
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cabangRestoranFragment = CabangRestoranFragment()
+        val testFragment = TestFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, cabangRestoranFragment)
+            commit()
+        }
+
+        val btnCabangRestoranFragment = findViewById<View>(R.id.btnCabangRestoran)
+        val btnTestFragment = findViewById<View>(R.id.btnTestFragment)
+
+        btnCabangRestoranFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, cabangRestoranFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        btnTestFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, testFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 }
