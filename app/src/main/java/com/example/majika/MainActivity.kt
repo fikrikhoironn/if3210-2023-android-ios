23447783a8acc1f17c69156f7935269b03e09888
fikrikhoironn <fikrikf470@gmail.com>
2023-02-14 13:53:48 +0700

    feat: navbar done

--- app/src/main/java/com/example/majika/MainActivity.kt
+++ app/src/main/java/com/example/majika/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.majika
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.majika.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.cabang_restoran -> {
+                    replaceFragment(CabangRestoranFragment())
+                    true
+                }
+                R.id.daftar_makanan -> {
+                    replaceFragment(DaftarMakananFragment())
+                    true
+                }
+                R.id.twibbon -> {
+                    replaceFragment(TwibbonFragment())
+                    true
+                }
+                R.id.keranjang -> {
+                    replaceFragment(KeranjangFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
