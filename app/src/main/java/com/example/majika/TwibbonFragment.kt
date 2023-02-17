f7a21148c4ce7b44bcfc0e5633a824094bc05b2c
jasonk19 <13520080@std.stei.itb.ac.id>
2023-02-17 12:43:17 +0700

    feat: add camera functionality in twibbon

--- app/src/main/java/com/example/majika/TwibbonFragment.kt
+++ app/src/main/java/com/example/majika/TwibbonFragment.kt
@@ -1,17 +1,135 @@
 package com.example.majika
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
+import android.hardware.camera2.params.SessionConfiguration
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Surface
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class TwibbonFragment : Fragment() {
+
+    lateinit var cameraManager: CameraManager
+    lateinit var textureView: TextureView
+    lateinit var cameraCaptureSession: CameraCaptureSession
+    lateinit var cameraDevice: CameraDevice
+    lateinit var captureRequest: CaptureRequest
+    lateinit var handler: Handler
+    lateinit var handlerThread: HandlerThread
+    lateinit var capReq: CaptureRequest.Builder
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_twibbon, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        get_permissions()
+
+        textureView = view.findViewById(R.id.textureView)
+        cameraManager = requireContext().getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler((handlerThread).looper)
+
+        textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
+                open_camera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
+
+            }
+
+            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
+
+            }
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun open_camera() {
+        cameraManager.openCamera(cameraManager.cameraIdList[0], object : CameraDevice.StateCallback() {
+            override fun onOpened(p0: CameraDevice) {
+                cameraDevice = p0
+
+                capReq = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                var surface = Surface(textureView.surfaceTexture)
+                capReq.addTarget(surface)
+
+                cameraDevice.createCaptureSession(listOf(surface), object: CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(p0: CameraCaptureSession) {
+                        cameraCaptureSession = p0
+                        cameraCaptureSession.setRepeatingRequest(capReq.build(), null, null)
+                    }
+
+                    override fun onConfigureFailed(p0: CameraCaptureSession) {
+                        TODO("Not yet implemented")
+                    }
+                }, handler)
+            }
+
+            override fun onDisconnected(p0: CameraDevice) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onError(p0: CameraDevice, p1: Int) {
+                TODO("Not yet implemented")
+            }
+        }, handler)
+    }
+
+    private fun get_permissions() {
+        var permissionList: MutableList<String> = mutableListOf<String>()
+
+        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(android.Manifest.permission.CAMERA)
+        }
+        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
+        if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+
+        if (permissionList.size > 0) {
+            ActivityCompat.requestPermissions(requireActivity(), permissionList.toTypedArray(), 101)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        grantResults.forEach {
+            if (it != PackageManager.PERMISSION_GRANTED) {
+                get_permissions()
+            }
+        }
     }
 }
