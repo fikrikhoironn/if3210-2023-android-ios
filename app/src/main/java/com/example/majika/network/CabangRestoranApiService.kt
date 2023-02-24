749ed81377e00516f17e9abecff0a9aaa34c7f28
jasonk19 <13520080@std.stei.itb.ac.id>
2023-02-24 13:32:36 +0700

    chore: add const to base url

--- app/src/main/java/com/example/majika/network/CabangRestoranApiService.kt
+++ app/src/main/java/com/example/majika/network/CabangRestoranApiService.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private val BASE_URL =  "http://192.168.56.1:3000/v1/"
+private const val BASE_URL =  "http://192.168.56.1:3000/v1/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
